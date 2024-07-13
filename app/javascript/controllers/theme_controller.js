7974273659af16a3993656bf61a12a65547d31d5
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-07-13 13:02:20 +0200

    js cleanup

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -4,25 +4,20 @@ import { Controller } from "@hotwired/stimulus";
  * This controller is responsible for switching between system, light and dark themes.
  */
 export default class extends Controller {
-    static targets = ["toggleButton"];
-
-    connect() {
-        this.loadThemePreference();
-    }
-
-    toggle() {
-        const mode = document.documentElement.classList.toggle("dark");
-        this.saveThemePreference(isDarkMode);
-    }
-
-    loadThemePreference() {
-        const isDarkMode = localStorage.getItem("themePreference") === "dark";
-        if (isDarkMode) {
-            document.documentElement.classList.add("dark");
+    initialize() {
+        if (localStorage.theme === 'dark') {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
         }
     }
 
-    saveThemePreference(mode) {
-        localStorage.setItem("themePreference", mode);
+    toggle() {
+        if (localStorage.theme === 'dark') {
+            localStorage.theme = 'light'
+        } else {
+            localStorage.theme = 'dark'
+        }
+        this.initialize()
     }
 }

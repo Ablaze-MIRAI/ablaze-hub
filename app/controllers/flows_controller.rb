f6759e13ad8f7870f96abf0efe34a19b2152116d
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-07-14 17:51:16 +0200

    working login and registration

--- app/controllers/flows_controller.rb
+++ app/controllers/flows_controller.rb
@@ -1,65 +1,22 @@
 class FlowsController < ApplicationController
 
   def registration
-    kratos_url = ENV['KRATOS_PUBLIC_URL']
     flow_id = params[:flow]
-
-    if flow_id
-      full_url = "#{kratos_url}/self-service/registration/flows?id=#{flow_id}"
-      response = fetch_content(full_url)
-      if response.status == 410
-        @error = JSON.pretty_generate(response.parse)
-        @flow_type = "registration"
-        render :error
-        return
-      end
-      if response.status == 400
-        redirect_to "/registration?flow=#{response.parse["id"]}"
-        return
-      end
-
-      if response.status == 303
-        redirect_to response.headers["Location"]
-        return
-      end
-
-      @submit_url = response.parse["ui"]["action"]
-      @flow_id = flow_id
-      @nodes = get_ui_nodes(response.body)
-      return
-    end
-
-    full_url = "#{kratos_url}/self-service/registration/browser?"
-    response = HTTP.get(full_url)
-    set_cookie(response)
-    redirect_to response.headers["Location"]
+    fetch_flow(flow_id, "registration", registration_path)
   end
 
   def registration_submit
     form_data = params
-    action_url = form_data["action_url"]
-    response = HTTP
-                 .headers("Content-Type" => "application/json")
-                 .headers("Cookie" => cookies.map { |k, v| "#{k}=#{v}=" }.join("; "))
-                 .post(action_url, :json => form_data)
-    if response.status == 200
-      redirect_to root_path
-    elsif response.status == 400
-      redirect_to "/registration?flow=#{response.parse["id"]}"
-    elsif response.status == 422
-      url = response.parse["redirect_browser_to"]
-      set_cookie(response)
-      @redirect_url = url
-      render :registration, status: 422
-    elsif response.status == 303
-      redirect_to response.headers["Location"]
-    else
-      @error = JSON.pretty_generate(response.parse)
-      @flow_type = "registration"
-    end
   end
 
   def login
+    flow_id = params[:flow]
+    fetch_flow(flow_id, "login", login_path)
+  end
+
+  def login_submit
+    form_data = params
+    handle_submit(form_data, "login")
   end
 
   def error
@@ -71,6 +28,60 @@ class FlowsController < ApplicationController
   end
 
   private
+
+  def fetch_flow(flow_id, flow_type, submit_path = nil)
+    kratos_url = ENV['KRATOS_PUBLIC_URL']
+    if flow_id
+      full_url = "#{kratos_url}/self-service/#{flow_type}/flows?id=#{flow_id}"
+      response = fetch_content(full_url)
+      if response.status == 410
+        @error = JSON.pretty_generate(response.parse)
+        @flow_type = flow_type
+        render :error
+        return
+      end
+      if response.status == 400
+        redirect_to "/#{flow_type}?flow=#{response.parse["id"]}"
+        return
+      end
+
+      if response.status == 303
+        redirect_to response.headers["Location"]
+        return
+      end
+
+      @flow = FlowResponse.new(response.parse, submit_path)
+      return
+    end
+
+    full_url = "#{kratos_url}/self-service/#{flow_type}/browser?"
+    response = HTTP.get(full_url)
+    set_cookie(response)
+    redirect_to response.headers["Location"]
+  end
+
+  def handle_submit(form_data, flow_type)
+    action_url = form_data["action_url"]
+    response = HTTP
+                 .headers("Content-Type" => "application/json")
+                 .headers("Cookie" => cookies.map { |k, v| "#{k}=#{v}=" }.join("; "))
+                 .post(action_url, :json => form_data)
+    if response.status == 200
+      redirect_to root_path
+    elsif response.status == 400
+      redirect_to "/#{flow_type}?flow=#{response.parse["id"]}"
+    elsif response.status == 422
+      url = response.parse["redirect_browser_to"]
+      set_cookie(response)
+      @redirect_url = url
+      render :registration, status: 422
+    elsif response.status == 303
+      redirect_to response.headers["Location"]
+    else
+      @error = JSON.pretty_generate(response.parse)
+      @flow_type = flow_type
+    end
+  end
 
   def set_cookie(response)
     response_headers = response.headers
@@ -85,8 +96,6 @@ class FlowsController < ApplicationController
   def get_ui_nodes(body)
     JSON.parse(body)["ui"]["nodes"]
   end
-
-  def process_response(response, flow_type) end
 
   def fetch_content(url)
     cookie_string = cookies.map { |k, v| "#{k}=#{v}=" }.join("; ")

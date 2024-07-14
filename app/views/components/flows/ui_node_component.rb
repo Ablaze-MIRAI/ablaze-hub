40e3ff6954a469e3068095e34484db2b489a643c
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-07-14 17:51:03 +0200

    working view components

--- app/views/components/flows/ui_node_component.rb
+++ app/views/components/flows/ui_node_component.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 class Flows::UiNodeComponent < ViewComponent::Base
-  def initialize(node:)
-    @node = node
-    @meta = node["meta"]
-    attributes = node["attributes"]
-    @name = attributes["name"]
-    @type = attributes["type"]
-    @required = attributes["required"]
-    @autocomplete = attributes["autocomplete"]
-    @disabled = attributes["disabled"]
-    @value = if attributes["value"] then attributes["value"] else nil end
-
-    @messages = node["messages"]
+  def initialize(node)
+    @messages = node.messages
+    @meta = node.meta
+    @node_type = node.node_type
+    @name = node.name
+    @type = node.type
+    @required = node.required
+    @autocomplete = node.autocomplete
+    @disabled = node.disabled
+    @value = node.value
+    @group = node.group
   end
-
 end

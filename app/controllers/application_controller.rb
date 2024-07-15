23cb8e19e132eabf7df5707ff76f55580e692862
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-07-15 14:24:37 +0200

    working session display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   def index
+    kratos_url = ENV['KRATOS_PUBLIC_URL']
+    cookie_str = cookies.map { |k, v| "#{k}=#{v}" }.join("; ")
+    session_response = HTTP
+                         .headers("Content-Type" => "application/json")
+                         .headers("Accept" => "application/json")
+                         .headers("Cookie" => cookie_str)
+                         .get("#{kratos_url}/sessions")
+
+    @sessions = []
+    if session_response.status == 200
+      @sessions = session_response.parse.map do |session|
+        SessionResponse.new(session)
+      end
+      @raw_sessions = JSON.pretty_generate(session_response.parse)
+    end
   end
 end

0c1fccf4adb688cc8bd79d0e732fc70b3ce3af34
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-07-16 21:06:21 +0200

    working ip forwarding

--- app/controllers/flows_controller.rb
+++ app/controllers/flows_controller.rb
@@ -65,6 +65,7 @@ class FlowsController < ApplicationController
     action_url = form_data["action_url"]
     response = HTTP
                  .headers("Content-Type" => "application/json")
+                 .headers("X-Forwarded-For" => request.remote_ip)
                  .headers("User-Agent" => request.user_agent)
                  .headers("Cookie" => cookies.map { |k, v| "#{k}=#{v}=" }.join("; "))
                  .post(action_url, :json => form_data)

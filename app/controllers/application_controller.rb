de75d72d58998df2ef370b5bf1ee034287ec7016
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-07-19 12:59:24 +0200

    cleaned up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,46 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :get_logout
   def index
-    kratos_url = ENV['KRATOS_PUBLIC_URL']
-    session_response = HTTP
-                         .headers("Content-Type" => "application/json")
-                         .headers("Accept" => "application/json")
-                         .headers("Cookie" => get_cookie_str)
-                         .get("#{kratos_url}/sessions")
 
-    @sessions = []
-    if session_response.status == 200
-      _sessions = session_response.parse
-      if _sessions.length == 0
-        cookies.delete("ory_kratos_session")
-        redirect_to "/"
-      end
-      @sessions = session_response.parse.map do |session|
-        SessionResponse.new(session)
-      end
-      @raw_sessions = JSON.pretty_generate(session_response.parse)
-    end
-  end
-
-  def whoami
-    kratos_url = ENV['KRATOS_PUBLIC_URL']
-    response = HTTP
-                 .headers("Content-Type" => "application/json")
-                 .headers("Content-Type" => "application/json")
-                 .headers("Cookie" => get_cookie_str)
-                 .get("#{kratos_url}/sessions/whoami")
-
-    if response.status == 200
-      @raw_response = JSON.pretty_generate(response.parse)
-    end
   end
 
   private
 
-  def get_sessions
-
+  def get_logout
+    kratos_url = ENV['KRATOS_PUBLIC_URL']
+    return_to = params[:return_to] || root_url
+    response = helpers.get("#{kratos_url}/self-service/logout/browser?return_to=#{return_to}")
+    if helpers.is_logged_in? && response.status == 200
+      @logout_token = response.parse["logout_token"]
+      @logout_url = "/logout?token=#{@logout_token}&return_to=#{return_to}"
+    end
   end
 
-  def get_cookie_str
-    cookies.map { |k, v| "#{k}=#{v}" }.join("; ")
-  end
 end

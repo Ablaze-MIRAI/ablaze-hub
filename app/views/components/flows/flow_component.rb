704e27dc6cc18020538bfe97e42bb04ed4f398ec
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-07-28 21:18:08 +0200

    fixed flow component to make organizing easier

--- app/views/components/flows/flow_component.rb
+++ app/views/components/flows/flow_component.rb
@@ -6,21 +6,19 @@ module Flows
 
     renders_many :items, types: {
       group: {
-        renders: ->(flow, **options) { build_group(flow, **options) },
+        renders: ->(**options) { build_group(**options) },
         as: :group
       },
       back: {
-        renders: ->(flow, **options) {
-          if flow["ui"]["action"].nil?
-            return
-          end
-          node = helpers.get_back_node(flow)
-
+        renders: ->(**options) {
+          node = helpers.get_back_node(@flow)
           unless node.present?
             return
           end
 
-          UiNodeComponent.new(node, **options)
+          default_nodes = filter_only(["default"], @nodes)
+          all_nodes = [node] + default_nodes
+          build_group(@flow, nodes: all_nodes, **options)
         },
         as: :back
       },
@@ -37,7 +35,10 @@ module Flows
 
     renders_many :messages
 
-    def initialize(**options)
+    def initialize(flow:, submit_path: nil, **options)
+      @flow = flow
+      @submit_path = submit_path
+      @nodes = flow["ui"]["nodes"]
       @options = options
       @options[:tag] ||= :div
       @options[:classes] = class_names("border border-gray-300 dark:border-gray-700 p-4 min-w-96 rounded-md", @options[:classes])
@@ -45,48 +46,85 @@ module Flows
       @ui_messages = []
     end
 
-    def build_group(flow, **options)
-      action = flow["ui"]["action"]
-      method = flow["ui"]["method"]
-      submit_path = options[:submit] || action
-      nodes = flow["ui"]["nodes"]
+    def build_group(nodes = nil, **options)
+      action = @flow["ui"]["action"]
+      method = @flow["ui"]["method"]
+      submit_path = @submit_path || action
+      show_back = options.delete(:show_back) || false
+      _options = options.dup
+      _options[:action] = action
+      _options[:method] = method
+      _options[:submit] = submit_path
+      _options[:show_back] = show_back
 
-      if nodes.nil? || nodes.empty?
+      if nodes.present?
+        return NodeGroupsComponent.new(nodes: nodes, hide_back: !show_back, **_options)
+      end
+
+      _nodes = @nodes.dup
+      if _nodes.nil? || _nodes.empty?
         return
       end
 
-      if options[:filter_only]
-        nodes = filter_only(options[:filter_only], nodes)
+      _filter_only = options.delete(:filter_only)
+      _filter_out = options.delete(:filter_out)
+
+      if _filter_only
+        _nodes = filter_only(_filter_only, _nodes)
       end
 
-      if options[:filter_out]
-        nodes = filter_out(options[:filter_out], nodes)
+      if _filter_out
+        _nodes = filter_out(_filter_out, _nodes)
       end
-
-      show_back = options[:show_back] || false
 
       # TODO: submit might missing
-      NodeGroupsComponent.new(nodes: nodes, action: action, submit: submit_path, method: method, hide_back: !show_back, **options)
+      NodeGroupsComponent.new(nodes: _nodes, hide_back: !show_back, **_options)
+    end
+
+    def has_back_node?
+      helpers.has_back_node?(@flow)
     end
 
     ##
     # Return only the groups that have nodes matching the given type
+    # It will include the 'default' group by default
     # @param [Array] groups Array of strings representing the group names
     # @return [Array] Array of Node objects
     def filter_only(groups, nodes)
+      has_node = false
       nodes.select do |node|
-        groups.include?(node["group"])
+        has_node = groups.include?(node["group"])
+        if has_node || node["group"] == "default"
+          true
+        end
+      end
+
+      unless has_node
+        []
       end
     end
 
     ##
     # Return only the groups that do not have nodes matching the given type
+    # It will include the 'default' group by default
     # @param [Array] groups Array of strings representing the group names
     # @return [Array] Array of Node objects
     def filter_out(groups, nodes)
-      nodes.reject do |node|
-        groups.include?(node["group"])
+      has_node = false
+      nodes.select do |node|
+        has_node = groups.include?(node["group"])
+        if !has_node || node["group"] == "default"
+          true
+        end
       end
+
+      if has_node
+        []
+      end
+    end
+
+    def has_groups(groups)
+      filter_only(groups, @nodes).any?
     end
   end
 end

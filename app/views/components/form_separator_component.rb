9b07efdd4022fdaf360cc948726255b7a3952b1e
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-07-28 21:18:53 +0200

    only line for separator

--- app/views/components/form_separator_component.rb
+++ app/views/components/form_separator_component.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class FormSeparatorComponent < ApplicationComponent
+  def initialize(text: "OR", **options)
+    @text = text
+    @only_line = options[:only_line] || false
+    @text = "|" if @only_line
+    @options = options
+  end
+
   def call
     content_tag(:div, class: "relative my-2") do
       concat(separator_line)
@@ -17,8 +24,10 @@ class FormSeparatorComponent < ApplicationComponent
   end
 
   def separator_text
+    bg = @only_line ? "bg-transparent" : "bg-background"
+    fg = @only_line ? "text-transparent" : "text-muted-foreground"
     content_tag(:div, class: "relative flex justify-center text-xs uppercase") do
-      content_tag(:span, "OR", class: "px-2 bg-background text-muted-foreground")
+      content_tag(:span, @text, class: "px-2 #{bg} #{fg}")
     end
   end
 end

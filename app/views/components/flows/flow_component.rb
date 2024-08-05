acfd97a072b8d0c653cb4bf30837096232f8903e
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-08-05 20:32:41 +0200

    refactored flow component for back button support

--- app/views/components/flows/flow_component.rb
+++ app/views/components/flows/flow_component.rb
@@ -16,9 +16,7 @@ module Flows
             return
           end
 
-          default_nodes = filter_only(["default"], @nodes)
-          all_nodes = [node] + default_nodes
-          build_group(@flow, nodes: all_nodes, **options)
+          build_group(@flow, nodes: [node], show_back: true, **options)
         },
         as: :back
       },
@@ -33,29 +31,33 @@ module Flows
       }
     }
 
-    renders_many :messages
-
     def initialize(flow:, submit_path: nil, **options)
       @flow = flow
-      @submit_path = submit_path
       @nodes = flow["ui"]["nodes"]
+      @action = flow["ui"]["action"]
+      @submit = submit_path || @action
+
       @options = options
-      @options[:tag] ||= :div
+      @options[:tag] ||= :form
+      @options[:method] ||= flow["ui"]["method"] || "post"
       @options[:classes] = class_names("border border-gray-300 dark:border-gray-700 p-4 min-w-96 rounded-md", @options[:classes])
       @show = options[:show_condition] || true
       @ui_messages = []
+
+      if @action == nil
+        raise "The action attribute is required for the NodeGroupsComponent"
+      end
+
+      if @submit != @action
+        @action_url = ApplicationComponent.new(tag: :input, type: :hidden, name: :action_url, value: @action)
+      else
+        @action_url = nil
+      end
     end
 
     def build_group(nodes = nil, **options)
-      action = @flow["ui"]["action"]
-      method = @flow["ui"]["method"]
-      submit_path = @submit_path || action
       show_back = options.delete(:show_back) || false
       _options = options.dup
-      _options[:action] = action
-      _options[:method] = method
-      _options[:submit] = submit_path
-      _options[:show_back] = show_back
 
       if nodes.present?
         return NodeGroupsComponent.new(nodes: nodes, hide_back: !show_back, **_options)
@@ -66,8 +68,8 @@ module Flows
         return
       end
 
-      _filter_only = options.delete(:filter_only)
-      _filter_out = options.delete(:filter_out)
+      _filter_only = _options.delete(:filter_only)
+      _filter_out = _options.delete(:filter_out)
 
       if _filter_only
         _nodes = filter_only(_filter_only, _nodes)
@@ -87,39 +89,21 @@ module Flows
 
     ##
     # Return only the groups that have nodes matching the given type
-    # It will include the 'default' group by default
     # @param [Array] groups Array of strings representing the group names
     # @return [Array] Array of Node objects
     def filter_only(groups, nodes)
-      has_node = false
       nodes.select do |node|
-        has_node = groups.include?(node["group"])
-        if has_node || node["group"] == "default"
-          true
-        end
-      end
-
-      unless has_node
-        []
+        groups.include?(node["group"])
       end
     end
 
     ##
     # Return only the groups that do not have nodes matching the given type
-    # It will include the 'default' group by default
     # @param [Array] groups Array of strings representing the group names
     # @return [Array] Array of Node objects
     def filter_out(groups, nodes)
-      has_node = false
       nodes.select do |node|
-        has_node = groups.include?(node["group"])
-        if !has_node || node["group"] == "default"
-          true
-        end
-      end
-
-      if has_node
-        []
+        !groups.include?(node["group"])
       end
     end
 
